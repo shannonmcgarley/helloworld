fa0d7394f34272dd541055a97f302255168b3580
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2019-01-31 17:30:50 +0000

    newFunction: added a class with an if statement and speak function

--- src/main/scala/greetings.scala
+++ src/main/scala/greetings.scala
@@ -1,10 +1,21 @@
 import scala.io.StdIn
 
+class Person(name: String) {
+
+  def speak(): String = {
+    if (name == "shannon") {
+      "you dont get a hello, bye felicia"
+    } else {
+      s"hello $name"
+
+    }
+  }
+}
+
 object greetings extends App {
 
-  def greet(name : String) = println(s"hello $name")
-
   val name = StdIn.readLine("what is your name? ")
+  val person = new Person(name)
+  println(person.speak())
 
-  greet(name)
 }
