0cb73237309513b47cdb9dde587745cd55df182a
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2019-01-31 11:20:10 +0000

    newFunction: greeter

--- src/main/scala/helloWorld.scala
+++ src/main/scala/helloWorld.scala
@@ -1,6 +1,12 @@
 object helloWorld extends App {
 
   val name = "shannon"
-println("hello " + name)
-  
+  val name2 = "pan"
+
+  def greeter(name: String) = {
+    println(s"hello $name")
+  }
+
+  greeter(name)
+  greeter(name2)
 }
