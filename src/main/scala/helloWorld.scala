9c1575024868ab9475bd0473d301b4473fb03621
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2019-01-31 12:15:16 +0000

    newFunction: if else and case match for messages

--- src/main/scala/helloWorld.scala
+++ src/main/scala/helloWorld.scala
@@ -1,8 +1,5 @@
 object helloWorld extends App {
-
-  val name = "shannon"
-  val pan = "pan"
-
+  
   def greeter(name: String, shout : Boolean) = {
    if (shout) {
      println(s"hello $name!")
@@ -11,6 +8,45 @@ object helloWorld extends App {
    }
   }
 
-  greeter(pan, true)
-  greeter(pan, false)
+//  greeter("pan", true)
+//  greeter("pan", false)
+
+  def personalGreeterold(names : String): Unit = {
+    if (names == "andrew") {
+      println("good morning andrew")
+    } else if (names == "dane") {
+      println("alright dave")
+    } else if (names == "dan"){
+      println("yo wazzup")
+    } else {
+      println(s"hello $names")
+    }
+  }
+
+  def personalGreeternewold(names : String): Unit = {
+      names match {
+        case "andrew" => println("good morning andrew")
+        case "dane"   => println("alright dave")
+        case "dan"    => println("yo wazzup")
+        case _        => println(s"hello $names")
+      }
+  }
+
+  def personalGreeter(names : String) = {
+    val message = names match {
+      case "andrew" => "good morning andrew"
+      case "dane"   => "alright dave"
+      case "dan"    => "yo wazzup"
+      case _        => s"hello $names"
+    }
+    println(message)
+  }
+
+  ///////////////////////////////
+  personalGreeter("shannon")
+  personalGreeter("pan")
+  personalGreeter("andrew")
+  personalGreeter("dane")
+  personalGreeter("dan")
+  //////////////////////////////////
 }
