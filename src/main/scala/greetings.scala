abe9fb4fbda97b0d243711418cef132aa17b240f
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2019-02-07 18:42:15 +0000

    newFunction: created a bank account which then has a savings and a cash isa savings function

--- src/main/scala/greetings.scala
+++ src/main/scala/greetings.scala
@@ -1,12 +1,12 @@
 import scala.io.StdIn
 
 object Prompt {
-  def ask(message : String) = StdIn.readLine(message)
+  def ask(message: String) = StdIn.readLine(message)
 }
 
-class Person(name: String, age : Int) {
+class Person(name: String, age: Int) {
 
-  private val years : String = if(age > 1) "years" else "year"
+  private val years: String = if (age > 1) "years" else "year"
 
   def speak(): String = {
     if (name == "shannon") {
@@ -25,4 +25,37 @@ object greetings extends App {
   val person = new Person(name, age.toInt)
   println(person.speak())
 
+}
+
+abstract class BankAccount(accountNumber : String, balance : Double) {
+  def withdraw(amount: Double): BankAccount
+
+  def deposit(amount: Double): BankAccount
+}
+
+final class SavingsAccount(accountNumber : String, balance : Double) extends BankAccount(accountNumber, balance){
+  override def withdraw(amount : Double) : BankAccount = {
+    if ((balance - amount) < 0) {
+      println(s"you have insufficient funds")
+      this
+    } else {
+      val deducted = balance - amount
+      println(s"balance after deductions $deducted")
+
+      new SavingsAccount(accountNumber, deducted)
+    }
+  }
+  override def deposit(amount : Double) : BankAccount = {
+    new SavingsAccount(accountNumber, balance + amount)
+  }
+}
+
+final class CashISASavingsAccount (accountNumber : String, balance : Double) extends BankAccount(accountNumber, balance){
+  override def withdraw(amount: Double) : BankAccount = {
+    println(s"try again in 3 years boo")
+    this
+  }
+  override def deposit(amount : Double) : BankAccount = {
+    new CashISASavingsAccount(accountNumber, balance + amount)
+  }
 }
