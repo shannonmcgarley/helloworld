b0656ca5bc5a6ea4e2fe0d66cbfbd5bf7ab3421d
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2019-02-11 17:27:00 +0000

    cashisa is moving up in the world (finally)

--- src/main/scala/app/one/greetings.scala
+++ src/main/scala/app/one/greetings.scala
@@ -9,16 +9,19 @@ object Prompt {
 
 object greetings extends App {
 
+  val cashISA = new CashISASavingsAccount("10101", 0.00)
+  val deposited = cashISA.deposit(1000.00)
+  val withdrawn = deposited.withdraw(200.00)
   val name = Prompt.ask("what is your name? ")
   val age = Prompt.ask("what is your age?")
-  val person = new Person(name, age.toInt)
+  val person = new Person(name, age.toInt, withdrawn)
   println(person.speak())
 
 }
 
 class Person(name: String, age: Int, private val bankAccount: BankAccount) {
 
-  def this(name : String, age : Int) = this(name, age, new SavingsAccount("12345", 0.00))
+  def this(name: String, age: Int) = this(name, age, new SavingsAccount("12345", 0.00))
 
   private def years: String = if (age > 1) "years" else "year"
 
@@ -26,20 +29,23 @@ class Person(name: String, age: Int, private val bankAccount: BankAccount) {
     if (name == "shannon") {
       "you dont get a hello, bye felicia"
     } else {
-      s"hello $name, you are $age $years old. \n You have ${bankAccount.balance} in your account"
+      s"hello $name, you are $age $years old. \n Your account details are: $bankAccount"
 
     }
   }
 }
 
-abstract class BankAccount(accountNumber : String, val balance : Double) {
+abstract class BankAccount(accountNumber: String, val balance: Double) {
+
+  override def toString: String = s"Account number: $accountNumber, balance: $balance"
+
   def withdraw(amount: Double): BankAccount
 
   def deposit(amount: Double): BankAccount
 }
 
-final class SavingsAccount(accountNumber : String, balance : Double) extends BankAccount(accountNumber, balance){
-  override def withdraw(amount : Double) : BankAccount = {
+final class SavingsAccount(accountNumber: String, balance: Double) extends BankAccount(accountNumber, balance) {
+  override def withdraw(amount: Double): BankAccount = {
     if ((balance - amount) < 0) {
       println(s"you have insufficient funds")
       this
@@ -50,17 +56,28 @@ final class SavingsAccount(accountNumber : String, balance : Double) extends Ban
       new SavingsAccount(accountNumber, deducted)
     }
   }
-  override def deposit(amount : Double) : BankAccount = {
+
+  override def deposit(amount: Double): BankAccount = {
     new SavingsAccount(accountNumber, balance + amount)
   }
 }
 
-final class CashISASavingsAccount (accountNumber : String, balance : Double) extends BankAccount(accountNumber, balance){
-  override def withdraw(amount: Double) : BankAccount = {
-    println(s"try again in 3 years boo")
+final class CashISASavingsAccount(accountNumber: String, balance: Double) extends BankAccount(accountNumber, balance) {
+
+  private val depositThreshold: Double = 200.00
+
+  override def withdraw(amount: Double): BankAccount = {
+    println(s"You cannot withdraw money yet. \n Try again in 3 years boo")
     this
   }
-  override def deposit(amount : Double) : BankAccount = {
-    new CashISASavingsAccount(accountNumber, balance + amount)
+
+  override def deposit(amount: Double): BankAccount = {
+    if (amount > depositThreshold) {
+      val difference = amount - depositThreshold
+      println(s"You cannot deposit more than £$depositThreshold. \n Excess : £$difference")
+      new CashISASavingsAccount(accountNumber, balance + depositThreshold)
+    } else {
+      new CashISASavingsAccount(accountNumber, balance + amount)
+    }
   }
 }
